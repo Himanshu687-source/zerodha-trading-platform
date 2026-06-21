f4bcc9fd0d589b3b0dd6aa1088ba65018ddc37a5
Himanshu687-source <himanshumishra1699@gmail.com>
2026-06-21 11:59:37 +0530

    Fix remaining eslint warnings in support components to allow Vercel cloud build to succeed

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -5,23 +5,23 @@ function Hero() {
         <section className='container-fluid' id="supportHero">
             <div className=' p-5 ' id="supportWrapper">
                 <h4>Support Portal </h4>
-                <a href=" ">Track Tickets</a>
+                <a href="#">Track Tickets</a>
             </div>
 
              <div className='row  m-3'>
                 <div className='col-6 p-5 '>
                     <h1 className='fs-3'>Search for an answer or browse help topics to create a ticket</h1>
                     <input placeholder='Eg.how do i activate F&O,why is my order getting rejected..'></input><br/>
-                    <a href="" >Track account opening</a>
-                    <a href="" style={{marginLeft: "25px"}}>Track segment activation </a>
-                    <a href="" style={{marginLeft: "25px"}}>Intraday margins </a>
-                    <a href="" >Kite user manual </a>
+                    <a href="#" >Track account opening</a>
+                    <a href="#" style={{marginLeft: "25px"}}>Track segment activation </a>
+                    <a href="#" style={{marginLeft: "25px"}}>Intraday margins </a>
+                    <a href="#" >Kite user manual </a>
                 </div>
                 <div className='col-6 p-5'>
                     <h1>Featured</h1>
                     <ol>
-                        <li><a href="">Surveillance measure on scrips - June 2025</a><br/></li>
-                        <li><a href="">Offer for sale (OFS) – June 2025</a></li>
+                        <li><a href="#">Surveillance measure on scrips - June 2025</a><br/></li>
+                        <li><a href="#">Offer for sale (OFS) – June 2025</a></li>
                     </ol>
                     
                 </div>
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -3,9 +3,6 @@ import React from 'react';
 import Hero from './Hero';
 import CreateTicket from './CreateTicket';
 
-import Navbav from '../Navbar';
-import Footer from '../Footer';
-
 function SupportPage() {
     return ( 
         <>
